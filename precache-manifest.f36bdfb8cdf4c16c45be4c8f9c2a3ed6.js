6c80d3af71870650d20f48db9344ff0dfcfdca3b
Youngtae Seo <ryan@cworkers.kr>
2020-09-04 14:47:18 +0900

    Initial commit - temporary website

---
+++ precache-manifest.f36bdfb8cdf4c16c45be4c8f9c2a3ed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057e70486540f3277e87a25b6a5ea96e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0387353050678aff4699",
+    "url": "/static/css/main.0062ff1d.chunk.css"
+  },
+  {
+    "revision": "a7e3f6d95a09593e5af7",
+    "url": "/static/js/2.9133c934.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9133c934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0387353050678aff4699",
+    "url": "/static/js/main.b2cb1223.chunk.js"
+  },
+  {
+    "revision": "7ca45910be32dadb3c07",
+    "url": "/static/js/runtime-main.80391ece.js"
+  },
+  {
+    "revision": "9d10f2b152db45711a52c88efe876388",
+    "url": "/static/media/logo.9d10f2b1.svg"
+  }
+]);
